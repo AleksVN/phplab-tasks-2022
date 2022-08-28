6fd079646196cfb062251f7566851c1652e482e9
AleksVN <aleks.vn89@gmail.com>
2022-08-28 21:15:40 +0300

    [WEB] added functions.php index.php

--- src/web/functions.php
+++ src/web/functions.php
@@ -1,17 +1,56 @@
 <?php
-/**
- * The $airports variable contains array of arrays of airports (see airports.php)
- * What can be put instead of placeholder so that function returns the unique first letter of each airport name
- * in alphabetical order
- *
- * Create a PhpUnit test (GetUniqueFirstLettersTest) which will check this behavior
- *
- * @param  array  $airports
- * @return string[]
- */
+
 function getUniqueFirstLetters(array $airports)
 {
-    // put your logic here
+    $letters = array_map(function ($item) {
+        return ucfirst(substr($item['name'], 0, 1));
+    }, $airports);
 
-    return ['A', 'B', 'C'];
+    sort($letters);
+
+    return array_unique($letters);
+
+}
+
+function filterByLetter(array $airports, $letter)
+{
+    $resAirports = [];
+    foreach ($airports as $airport) {
+        if ($airport['name'][0] === $letter) {
+            $resAirports[] = $airport;
+        }
+    }
+
+    return $resAirports;
+}
+
+function filterByState(array $airports, $state)
+{
+    $resAirports = [];
+    foreach ($airports as $airport) {
+        if ($airport['state'] === $state) {
+            $resAirports[] = $airport;
+        }
+    }
+
+    return $resAirports;
+}
+
+function sortByField(array &$airports, $field)
+{
+    usort($airports, function ($a, $b) use ($field) {
+        if ($a[$field] == $b[$field]) {
+            return 0;
+        }
+
+        return ($a[$field] < $b[$field]) ? -1 : 1;
+    }
+    );
+}
+
+function addParams(...$params)
+{
+    $params = '?' . http_build_query(array_merge($_GET, ...$params));
+
+    return $params;
 }
